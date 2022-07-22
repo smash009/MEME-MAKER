7f3c8dde9ee02c03d8a72a0faffabcbc28a5c82a
smash009 <smash009@naver.com>
2022-07-23 02:05:55 +0900

    Update

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("3d");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+
+
+ctx.fillRect(50, 50, 100, 200); // 가로, 세로 50px 위치에 100 X 200 사각형
