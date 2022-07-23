6ce8c19a866dd41c21ef00f24509a91434e0cde8
smash009 <smash009@naver.com>
2022-07-23 11:58:42 +0900

    Update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 1;
 
 
 // ctx.fillRect(50, 50, 100, 200); // 가로, 세로 50px 위치에 100 X 200 사각형
@@ -22,15 +23,35 @@ canvas.height = 800;
 // ctx.lineTo(450, 200);
 // ctx.fill();
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+// ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+// ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+// ctx.fillRect(260 - 40, 200 - 30, 60, 200);
 
-ctx.arc(250, 100, 50, 0, 2 *  Math.PI);
-ctx.fill();
+// ctx.arc(250, 100, 50, 0, 2 *  Math.PI);
+// ctx.fill();
 
-ctx.beginPath(); // 경로를 시작하거나 현재 경로를 재설정
-ctx.fillStyle = "white";
-ctx.arc(260 + 10, 80, 8, 0, Math.PI, 2 * Math.PI );
-ctx.arc(220 + 10, 80, 8, 0, Math.PI, 2 * Math.PI);
-ctx.fill();
+// ctx.beginPath(); // 경로를 시작하거나 현재 경로를 재설정
+// ctx.fillStyle = "white";
+// ctx.arc(260 + 10, 80, 8, 0, Math.PI, 2 * Math.PI );
+// ctx.arc(220 + 10, 80, 8, 0, Math.PI, 2 * Math.PI);
+// ctx.fill();
+
+const colors = [
+    "#ff9ff3", "#feca57", "#ff6b6b", "#48dbfb", "#1dd1a1", "#f368e0", "#ff9f43", "#ee5253", "#0abde3", "#10ac84", "#c8d6e5", "#222f3e"
+];
+
+function onclick(e) {
+
+    console.log(e.offsetX, e.offsetY);
+    
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+}
+
+canvas.addEventListener("click", onclick);
