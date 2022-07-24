94a2e09a043f1686ada02a246c60650bbf214de4
smash009 <smash009@naver.com>
2022-07-24 15:33:18 +0900

    Update

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const modeBtn = document.getElementById("mode-btn");
+const resetBtn = document.getElementById("reset-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 // const colorOptions = document.getElementsByClassName("color-option"); // 배열이 아님.
-const colorOptions = Array.from(document.getElementsByClassName("color-option")); // 베열로 생성
+const colorOptions = Array.from(document.getElementsByClassName("color-option")); // 베열 생성
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
-ctx.lineWidth = lineWidth.value; // 초기값
+
+const CANVAS_WIDTH = 800; // 
+const CANVAS_HEIGHT = 800; // 
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value; // 초기값, 한번만 실행
 let isPainting = false;
 let isFilling = false;
 
@@ -57,17 +63,47 @@ function onColorClick(e) { // 이벤트가 들어가야 하는 것 염두.
     color.value = colorValue; // 위에서 바꾼 값을 input에도 변경해서 넣어줌
 }
 
+function onModeClick() { // fill draw 모드 전환
+    //console.log(modeBtn);
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
 
+function onCanvasClick() { // fill 모드일 때 지정된 색상을 캔버스 전체에 덮음
+    if(isFilling) {
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+
+function onResetClick() {
+    ctx.fillStyle = "#fff"; // 리셋의 개념은 기본 바탕색인 흰색으로 덮으면 됨
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+    ctx.strokeStyle = "#fff"; // 지운다의 개념은 흰바탕 색과 같이 그려주는 것
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("mousedown", onCanvasClick); // Click -> mousedown 으로 수정(클릭한 채로 드래그하면 라인이 그려져서)
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
-colorOptions.forEach(color => color.addEventListener("click", onColorClick)); // 어떤 색상이 클릭 되었는지 감지.
+colorOptions.forEach(color => color.addEventListener("click", onColorClick)); // 어떤 색상이 클릭 되었는지 감지. ForEach 함수 사용하려고 배열로 변환
 
+modeBtn.addEventListener("click", onModeClick);
+resetBtn.addEventListener("click", onResetClick);
+eraserBtn.addEventListener("click", onEraserClick);
 
 
 
